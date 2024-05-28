ab9ce068015baeb8e69e6f202969dfe8f1c8e0de
Siddheshvar-2802 <mrsiddheshvar.patil@gmail.com>
2024-05-29 00:51:07 +0530

    API Constants

---
+++ src/main/java/com/EmployeeManagementSystem/utils/APIConstant.java
@@ -0,0 +1,27 @@
+package com.EmployeeManagementSystem.utils;
+
+public class APIConstant {
+
+    public static final String BASE_REQUEST  = "/api";
+
+    public static final String USER_SERVICE = "/user";
+    public static final String CREATE_USER = "/create";
+    public static final String GET_USER_BY_ID = "/get/{id}";
+    public static final String GET_ALL_USERS = "/getAll";
+    public static final String UPDATE_USER_BY_ID = "/update/{id}";
+    public static final String DELETE_USER_BY_ID = "/delete/{id}";
+
+    public static final String DESIGNATION_SERVICE = "/designation";
+    public static final String CREATE_DESIGNATION = "/create";
+    public static final String GET_DESIGNATION_BY_ID = "/get/{id}";
+    public static final String GET_ALL_DESIGNATIONS = "/getAll";
+    public static final String UPDATE_DESIGNATION_BY_ID = "/update/{id}";
+    public static final String DELETE_DESIGNATION_BY_ID = "/delete/{id}";
+
+    public static final String DEPARTMENT_SERVICE = "/department";
+    public static final String CREATE_DEPARTMENT = "/create";
+    public static final String GET_DEPARTMENT_BY_ID = "/get/{id}";
+    public static final String GET_ALL_DEPARTMENTS = "/getAll";
+    public static final String UPDATE_DEPARTMENT_BY_ID = "/update/{id}";
+    public static final String DELETE_DEPARTMENT_BY_ID = "/delete/{id}";
+}
